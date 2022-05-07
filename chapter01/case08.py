fdf1dfa281da06a91f6342ce253311fc628b8b2b
cuiweizhi <330459539@qq.com>
2022-05-07 15:51:42 +0800

    Update a file of chapter01/case08.py

--- chapter01/case08.py
+++ chapter01/case08.py
@@ -32,14 +32,14 @@ squared = [[x ** 2 for x in row] for row in matrix]
 print(squared)
 print('------------------------------------')
 
-# Example three
+# Example Three
 a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 b = [x for x in a if x > 4 if x % 2 == 0]   # 两种推导方式等效
 c = [x for x in a if x > 4 and x % 2 == 0]
 print(b == c)
 print('------------------------------------')
 
-# Example four
+# Example Four
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 filtered = [[x for x in row if x % 3 == 0] for row in matrix if sum(row) >= 10]
 print(filtered)
