3173297bb96016f279777e4794e70a11ad70fcf4
cuiweizhi <330459539@qq.com>
2022-05-07 16:02:49 +0800

    Update a file of chapter01/case07.py

--- chapter01/case07.py
+++ chapter01/case07.py
@@ -42,8 +42,8 @@ assert even_squares == list(alt)
 print('---------------------------')
 
 # Example five
-chile_ranks = {'ghost': 1, 'hero':2, 'cayenne': 3}
-rank_dict = {rank: name for name , rank in chile_ranks.items()}
+chile_ranks = {'ghost': 1, 'hero': 2, 'cayenne': 3}
+rank_dict = {rank: name for name, rank in chile_ranks.items()}
 chile_len_set = {len(name) for name in rank_dict.values()}
 print(rank_dict)
 print(chile_len_set)
